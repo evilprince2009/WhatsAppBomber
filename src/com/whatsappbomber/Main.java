b04c3e06b142cf8cad5c6905e29c60e4f052753b
Ibne Nahian <evilprince2009@gmail.com>
2021-10-14 13:21:46 +0600

    basic implimantation

--- src/com/whatsappbomber/Main.java
+++ src/com/whatsappbomber/Main.java
@@ -1,8 +1,36 @@
 package com.whatsappbomber;
 
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws AWTException, InterruptedException {
+        Scanner buffer = new Scanner(System.in);
+        Robot robot = new Robot();
+
+        System.out.println("Enter your spam text");
+        String message = buffer.nextLine();
+        System.out.println("Enter how many times you want to spam");
+        int times = buffer.nextInt();
+        StringSelection selectedString = new StringSelection(message);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(selectedString, null);
+        System.out.println("Bombing starts in 2 seconds ...!");
+        Thread.sleep(5000);
+        for (int i = 0; i < times; i++) {
+            Thread.sleep(5000);
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            robot.keyPress(KeyEvent.VK_V);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            robot.keyRelease(KeyEvent.VK_V);
+            robot.keyPress(KeyEvent.VK_ENTER);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+        }
     }
+
+    
 }
